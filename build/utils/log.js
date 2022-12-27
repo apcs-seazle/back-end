cd4667738ae41e9d7ed80864dc9cd7ddcfa7a781
vbph <vbphung20@apcs.fitus.edu.vn>
2022-12-28 00:28:30 +0700

    remove build folder

--- build/utils/log.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const log = (tit, ...msgs) => {
-    console.log(`[seazle] ${tit}:`, ...msgs);
-};
-exports.default = log;
